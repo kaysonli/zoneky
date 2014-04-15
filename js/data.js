0ad754c77665ed1a684f7c1aca0b597c018ead89
Kayson Li <kayson.li@morningstar.com>
2014-04-15 08:50:14 +0800

    add fake data

---
+++ js/data.js
@@ -0,0 +1,141 @@
+window.dataSource = {
+    "status": "ok",
+    "data": {
+        "lineGroups": [{
+            "id": "FC791C05-604C-4131-AEE3-11BD797E82A6",
+            "lines": [{
+                "percentile": "95th",
+                "points": [{
+                    "x": 0,
+                    "y": 1
+                }, {
+                    "x": 1,
+                    "y": 1.15233
+                }]
+            }, {
+                "percentile": "50th",
+                "points": [{
+                    "x": 0,
+                    "y": 1
+                }, {
+                    "x": 1,
+                    "y": 1.07043
+                }]
+            }, {
+                "percentile": "5th",
+                "points": [{
+                    "x": 0,
+                    "y": 1
+                }, {
+                    "x": 1,
+                    "y": 1.00875
+                }]
+            }]
+        }],
+        "gridData": [{
+            "amId": "63C828CA-A1A0-4353-9861-55251AF01DEA",
+            "projectedYear": 5,
+            "95_0": 30.15,
+            "50_0": 9.5,
+            "5_0": 2.5
+        }, {
+            "amId": "63C828CA-A1A0-4353-9861-55251AF01DEA",
+            "projectedYear": 10,
+            "95_0": 190.15,
+            "50_0": 9.5,
+            "5_0": 2.5
+        }, {
+            "amId": "63C828CA-A1A0-4353-9861-55251AF01DEA",
+            "projectedYear": 20,
+            "95_0": 30.15,
+            "50_0": 9.5,
+            "5_0": 2.5
+        }],
+        "aggrBars": [{
+            "projectedYear": 5,
+            "groupData": [{
+                "id": "63C828CA-A1A0-4353-9861-55251AF01DEA",
+                "data": {
+                    "start": 58.972747776013954,
+                    "end": 99.39156762896307,
+                    "data": [{
+                        "percentile": "95th",
+                        "value": 99.39156762896307
+                    }, {
+                        "percentile": "50th",
+                        "value": 73.4812232181041
+                    }, {
+                        "percentile": "5th",
+                        "value": 58.972747776013954
+                    }]
+                }
+            }, {
+                "id": "5F8BD670-3398-402B-9162-3F723FF83ADE",
+                "data": [{
+                    "percentile": "95th",
+                    "value": 99.39156762896307
+                }, {
+                    "percentile": "50th",
+                    "value": 73.4812232181041
+                }, {
+                    "percentile": "5th",
+                    "value": 58.972747776013954
+                }]
+            }]
+        }, {
+            "projectedYear": 10,
+            "groupData": [{
+                "id": "63C828CA-A1A0-4353-9861-55251AF01DEA",
+                "data": [{
+                    "percentile": "95th",
+                    "value": 99.39156762896307
+                }, {
+                    "percentile": "50th",
+                    "value": 73.4812232181041
+                }, {
+                    "percentile": "5th",
+                    "value": 58.972747776013954
+                }]
+            }, {
+                "id": "5F8BD670-3398-402B-9162-3F723FF83ADE",
+                "data": [{
+                    "percentile": "95th",
+                    "value": 99.39156762896307
+                }, {
+                    "percentile": "50th",
+                    "value": 73.4812232181041
+                }, {
+                    "percentile": "5th",
+                    "value": 58.972747776013954
+                }]
+            }]
+        }, {
+            "projectedYear": 20,
+            "groupData": [{
+                "id": "63C828CA-A1A0-4353-9861-55251AF01DEA",
+                "data": [{
+                    "percentile": "95th",
+                    "value": 99.39156762896307
+                }, {
+                    "percentile": "50th",
+                    "value": 73.4812232181041
+                }, {
+                    "percentile": "5th",
+                    "value": 58.972747776013954
+                }]
+            }, {
+                "id": "5F8BD670-3398-402B-9162-3F723FF83ADE",
+                "data": [{
+                    "percentile": "95th",
+                    "value": 99.39156762896307
+                }, {
+                    "percentile": "50th",
+                    "value": 73.4812232181041
+                }, {
+                    "percentile": "5th",
+                    "value": 58.972747776013954
+                }]
+            }]
+        }]
+    }
+}
